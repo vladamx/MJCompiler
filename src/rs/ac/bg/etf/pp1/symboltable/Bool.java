3e8a4d1151c6b9f14d9fa166c8bf0f6b67209862
Vladimir Milojevic <vladamx.dev@gmail.com>
2019-01-12 02:05:13 +0100

    Support for bool type - Add untracked files.

---
+++ src/rs/ac/bg/etf/pp1/symboltable/Bool.java
@@ -0,0 +1,5 @@
+package rs.ac.bg.etf.pp1.symboltable;
+
+public class Bool {
+    public static BoolStruct struct = new BoolStruct();
+}
---
+++ src/rs/ac/bg/etf/pp1/symboltable/BoolStruct.java
@@ -0,0 +1,9 @@
+package rs.ac.bg.etf.pp1.symboltable;
+
+import rs.etf.pp1.symboltable.concepts.Struct;
+
+class BoolStruct extends Struct {
+     BoolStruct() {
+        super(5);
+    }
+}
